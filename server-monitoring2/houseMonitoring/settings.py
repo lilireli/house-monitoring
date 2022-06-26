7152a78355ff614bdfb0cc5f0a4a90b02eed3152
lilireli <aurelie.suzanne@orange.fr>
2022-06-26 17:50:19 +0200

    turned debug off

--- server-monitoring2/houseMonitoring/settings.py
+++ server-monitoring2/houseMonitoring/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f0kh2n!+m$fq5v_s(cj@pt&3r8ac2$w*tqj3f&ce&7p7%d)_(!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*.herokuapp.com']
 
