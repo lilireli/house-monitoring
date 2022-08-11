819b6c01565001c74ae6008ebe8bff64264ef5d2
lilireli <aurelie.suzanne@orange.fr>
2022-08-11 11:29:43 +0200

    roll back zmq

--- server-monitoring2/temperature/sensor_communication.py
+++ server-monitoring2/temperature/sensor_communication.py
@@ -10,22 +10,18 @@ class SensorCommunication(threading.Thread):
         threading.Thread.__init__(self)
 
     def initialize_socket(self):
-        logging.info("Starting ZMQ thread")
+        logging.warning("Starting ZMQ thread")
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REP)    
         self.socket.bind("tcp://*:6999")
 
     def run(self):
-        try:
-            self.initialize_socket()
+        self.initialize_socket()
 
-            while 1:
-                #  Wait for next request from client
-                message = self.socket.recv()
-                logging.warning("Received request: %s" % message)
+        while 1:
+            #  Wait for next request from client
+            message = self.socket.recv()
+            logging.warning("Received request: %s" % message)
 
-                #  Send reply back to client
-                self.socket.send(b"World")
-        
-        except zmq.error.ZMQError:
-            logging.warning("Could not start socket")
+            #  Send reply back to client
+            self.socket.send(b"World")
