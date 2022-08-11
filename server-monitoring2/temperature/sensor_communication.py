45a943727ab15fa381d97222280c8e981224d772
lilireli <aurelie.suzanne@orange.fr>
2022-08-11 14:01:48 +0200

    server communicating alone

--- server-monitoring2/temperature/sensor_communication.py
+++ server-monitoring2/temperature/sensor_communication.py
@@ -27,3 +27,32 @@ class SensorCommunication(threading.Thread):
 
             #  Send reply back to client
             self.socket.send(b"World")
+
+class Sensor(threading.Thread):
+    def __init__(self):
+        self.context = None
+        self.socket = None
+        threading.Thread.__init__(self)
+
+    def initialize_socket(self):
+        port = 8000
+        logging.warning(f"Starting ZMQ thread on port {port}")
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REQ)    
+        self.socket.connect(f"ws://localhost:{port}")
+
+    def run(self):
+        self.initialize_socket()
+        request = 1
+
+        while 1:
+            print("Sending request %s …" % request)
+            self.socket.send(b"Hello")
+
+            #  Get the reply.
+            message = self.socket.recv()
+            print("Received reply %s [ %s ]" % (request, message))
+
+            request += 1
+
+            time.sleep(1)
